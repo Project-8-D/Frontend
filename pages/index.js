f1a36ee44762d9379e46dda38eaf2b6cff6593d4
gllms <willemvanderdoe@hotmail.com>
2022-04-22 09:29:37 +0200

    Add map

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import dynamic from "next/dynamic";
+
+// list of latitudes and longitudes
+const coords = [[51.626, 4.2], [51.624, 4.198], [51.624, 4.202]];
+
 export default function Home() {
+  const Map = dynamic(
+    () => import("../components/Map"),
+    {
+      loading: () => <p>Loading...</p>,
+      ssr: false
+    }
+  )
+
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <Map coords={coords} />
+    </>
   )
 }
