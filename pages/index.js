f6df395122e35e3a9741e29569cb137ddd66a628
Ovvugt <oscar.vugt@gmail.com>
2022-05-20 18:10:29 +0200

    Setup for login ui

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 export default function Home() {
-  fetch('http://localhost:5235/api/test')
-    .then(response => response.json())
-    .then(data => console.log(data))
   return (
     <h1>Home</h1>
   )
