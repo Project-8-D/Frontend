75f6aaa6cc7de403e6bb5632b1db4cc083f081f7
Ovvugt <51384692+Ovvugt@users.noreply.github.com>
2022-05-25 03:52:54 +0200

    Login UI update

--- pages/login.js
+++ pages/login.js
@@ -21,16 +21,16 @@ export default class Login extends Component {
     }
     render() {
         return (
-            <div className="block mx-auto">
+            <div className="block mx-auto w-fit">
                 <div className="max-w-2xl mt-[20vh]">
-                    <h1 className="text-8xl">Chengeta login</h1>
-                    <div className="py-3 block mx-auto max-w-[400px]">
+                    <h1 className="text-7xl">Chengeta login</h1>
+                    <div className="my-16 block mx-auto max-w-[400px]">
                         <form className="block max-w-[400px]" onSubmit={this.handleSubmit}>
-                            <input className="w-[100%] h-10 my-4 rounded-t-sm border-b border-gray-400 focus:border-b-[2px] focus:border-pink-500 focus:outline-none" type="email" name="email" placeholder="Email" required onChange={this.handleChange} />
-                            <input className="w-[100%] h-10 my-4 rounded-t-sm border-b border-gray-400 focus:border-b-[2px] focus:border-pink-500 focus:outline-none" type="password" name="password" placeholder="Password" required onChange={this.handleChange} />
-                            <div className="block mx-auto w-fit">
-                                <button className="border border-gray-600 text-pink-500 py-1 px-2 rounded-md mr-5">FORGOT PASSWORD</button>
-                                <button className="rounded-md text-gray-800 bg-pink-500 py-1 px-2 ml-5">LOGIN</button>
+                            <input type="email" name="email" placeholder="Email" required onChange={this.handleChange} />
+                            <input type="password" name="password" placeholder="Password" required onChange={this.handleChange} />
+                            <div className="block mx-auto w-fit my-8">
+                                <button className="font-semibold text-lg border border-gray-600 text-pink-500 py-2 px-4 rounded-md mr-10">FORGOT PASSWORD</button>
+                                <button className="font-semibold text-lg rounded-md text-gray-800 bg-pink-500 py-2 px-4 ml-10">LOGIN</button>
                             </div>
                         </form>
                     </div>
